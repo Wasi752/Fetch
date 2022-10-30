63e683cefac09d56c9e7f53fbeec11b0eb17c0cc
https://github.com/Wasi752 <osiur2@gmail.com>
2022-10-30 22:57:28 +0600

    Fetch in React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [arabic, setArabic] = useState();
+  fetch('https://raw.githubusercontent.com/Balagha/dhikr/main/public/data/dhikr.json')
+.then((response) => response.json())
+.then((data) => {
+  const s = data.map(x => x.ARABIC);
+  setArabic(s);
+});
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {arabic}
     </div>
   );
 }
