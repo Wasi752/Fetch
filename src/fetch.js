e350a804e4497da1f5844b389be240c66b7d0fc0
https://github.com/Wasi752 <osiur2@gmail.com>
2022-11-02 08:30:01 +0600

    Try to Formating

--- src/fetch.js
+++ src/fetch.js
@@ -2,15 +2,22 @@ import { useState } from 'react';
 
 function Fetch(){
 const [arabic, setArabic] = useState();
+const [banglao, setBanglao] = useState();
+const [bangla, setBangla] = useState();
+
   fetch('https://raw.githubusercontent.com/Balagha/dhikr/main/public/data/dhikr.json')
 .then((response) => response.json())
 .then((data) => {
-  const s = data.map(x => x.ARABIC);
-  setArabic("" + s + "");
-});
+  const s = data.map((x, i) => i+1 + data[i].ARABIC + data[i].BANGLA_UCCHARON + data[i].BANGLA_ORTHO + "");
+  setArabic, setBanglao, setBangla("" + s + "");
+}); 
 return (
-<div>
-    {arabic}
+<div className='w-full h-full'>
+  <div className='flex flex-row w-full h-full'>
+     <div className='w-2/12 h-full'></div>   
+     <div className='w-8/12 h-full'>{arabic} {banglao} {bangla}</div>
+     <div className='w-2/12 h-full'></div>
+    </div>
 </div>
 )
 }
