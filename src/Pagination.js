58940df53ea62880b3a550112919e6e45d53a566
Wasiur Rahman <osiur2@gmail.com>
2022-11-05 21:39:04 +0600

    No Change

--- src/Pagination.js
+++ src/Pagination.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 function Pagination() {
   const [bangla, setBangla] = useState();
   const [pageNo, setPageNo] = useState(1);
-  const [serial, setSerial] = useState(1)
+  const [counter, setCounter] = useState(5)
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/Balagha/dhikr/main/public/data/dhikr.json')
@@ -33,29 +33,29 @@ function Pagination() {
       </div>
       <div className='w-10/12 h-full flex'>
 
-        <button className='w-4/12 h-full rounded rounded-full bg-yellow-500 m-40 p-5 text-2xl'
+        <button className='w-2/12 h-full rounded rounded-full bg-yellow-500 mt-10 ml-56 p-5 text-2xl'
           onClick={() => setPageNo(pageNo - 1)} >
           Back
         </button>
 
 
-        <button className='w-4/12 h-full rounded rounded-full bg-blue-500 m-40 p-5 text-2xl'
+        <button className='w-2/12 h-full rounded rounded-full bg-blue-500 mt-10 ml-96 p-5 text-2xl'
           onClick={() => setPageNo(pageNo + 1)} >
           Next
         </button>
       </div>
       <div className='flex ml-40'>
-        <button className='w-1/12 h-full rounded rounded-full bg-red-500 m-10 p-5 text-2xl'
-          onClick={() => setSerial(serial[pageNo + 1])} >
+        <button className='w-2/12 h-full rounded rounded-full bg-red-500 mt-10 ml-20 p-5 text-2xl'
+          onClick={() => setPageNo(pageNo + 1)} >
           5
         </button>
 
-        <button className='w-1/12 h-full rounded rounded-full bg-pink-500 m-10 p-5 text-2xl'
+        <button className='w-2/12 h-full rounded rounded-full bg-pink-500 mt-10 ml-20 p-5 text-2xl'
           onClick={() => setPageNo(pageNo + 1)} >
           10
         </button>
 
-        <button className='w-1/12 h-full rounded rounded-full bg-green-500 m-10 p-5 text-2xl'
+        <button className='w-2/12 h-full rounded rounded-full bg-green-500 mt-10 ml-20 p-5 text-2xl'
           onClick={() => setPageNo(pageNo + 1)} >
           20
         </button>
